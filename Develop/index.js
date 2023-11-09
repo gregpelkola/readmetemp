3657fad55930f6d723b5517e76283f211fe7268c
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2023-11-08 20:16:03 -0600

    comments

--- Develop/index.js
+++ Develop/index.js
@@ -64,12 +64,12 @@ const questions = [
     
 
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 function writeToFile(fileName, data) {
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app
+// function to initialize app
 function init() {
     inquirer.prompt(questions).then((responses) => {
         console.log("Creating README.md File...");
